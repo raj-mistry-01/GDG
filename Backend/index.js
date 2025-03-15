9d4df7ec4ee251120421102269e0a069c0e9f95b
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-03-15 21:37:31 +0530

    9th

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import {app} from "./app.js";
 
 const port = 7000
 app.listen(port, () => {
-    console.log(`Server started on port: ${PORT}`);
+    console.log(`Server started on port: ${port}`);
 });
 
 app.get("/test", (req, res) => {
