bf180ddc23b2400cbad2fe42a1fe6b12f97e0267
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-03-15 21:48:31 +0530

    12th

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,6 +5,6 @@ app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
 
-app.get("/test", (req, res) => {
+app.get("/api/test", (req, res) => {
     res.send("Welcome to MatterAssist");
   });
