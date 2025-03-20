51c67a8723ded235b3787ecc0592acb16a384a44
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-03-20 11:25:10 +0530

    25th

--- python_processing/prediction.py
+++ python_processing/prediction.py
@@ -137,3 +137,4 @@ def predict_yield(crop, season, state, area, production, annual_rainfall, fertil
 if __name__ == "__main__":
     result = predict_yield('Arecanut', 'Whole Year', 'Assam', 73814 ,56708 , 7024878.38  ,22882.34 , 2051.4)
     print(f"Prediction: {result}")
+    #result = predict_yield('Arecanut', 'Whole Year', 'Assam', 73814, 56708, 2051, 7024878, 22882)
